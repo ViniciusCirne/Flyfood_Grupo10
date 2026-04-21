784a936fa75fefca98a9e312e5e62492c0922586
IcaroOm <icaroomdigital@gmail.com>
2026-04-21 13:38:29 -0300

    Adiciona função de permutação

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ def read_input():
                 delivery[char] = (row, col)
     return delivery, start
 
+
+def permutations(elements):
+    # Permutação feita recursivamente, usa o yield para ter um uso menor de memória
+    if len(elements) <= 1:
+        yield elements
+    else:
+        for p in permutations(elements[1:]):
+            for i in range(len(elements)):
+                yield p[:i] + elements[0:1] + p[i:]
+
+
 def main():
     # Lê o input
     delivery, start = read_input()
